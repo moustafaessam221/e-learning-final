cbb3505a1714a0706784f8db55cd711ee2dd82ae
moustafaessam221 <moustafaessam991@gmail.com>
2024-09-21 13:19:48 +0300

    Initializing the project

---
+++ src/pages/Courses.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+function Courses() {
+  return (
+    <h1>Courses Page</h1>
+  );
+}
+
+export default Courses;
