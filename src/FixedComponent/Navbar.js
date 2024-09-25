b1cfbab8cb805f48829dddf1ecaec95d777985ab
Sherif Othman <sherif.othman@sci.alexu.edu.eg>
2024-09-25 16:27:00 +0100

    Updated Navbar with icons for search, wishlist, and notifications

--- src/FixedComponent/Navbar.js
+++ src/FixedComponent/Navbar.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { FaUser, FaShoppingCart } from "react-icons/fa"; 
-import SearchBar from "./Search";
+import { Navbar, Nav, Container, Form, FormControl, Button } from "react-bootstrap";
+import { FaUser, FaShoppingCart, FaHeart, FaBell, FaSearch } from "react-icons/fa"; 
 
 const Navigationbar = () => {
   return (
@@ -17,7 +16,7 @@ const Navigationbar = () => {
         <Navbar.Toggle aria-controls="navbarScroll" />
 
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="ms-auto my-2 my-lg-0" navbarScroll>
+          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
             <LinkContainer to="/">
               <Nav.Link className="text-white" style={{ transition: "color 0.3s ease" }}>
                 Home
@@ -45,21 +44,43 @@ const Navigationbar = () => {
             </LinkContainer>
           </Nav>
 
-          <div className="d-flex justify-content-center mx-auto">
-            <SearchBar />
-          </div>
+          <Form className="d-flex mx-auto" style={{ maxWidth: "300px" }}>
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="light" style={{ backgroundColor: "#f8f9fa", border: "none" }}>
+              <FaSearch />
+            </Button>
+          </Form>
 
-          <LinkContainer to="/cart">
-            <Nav.Link className="text-white mx-2" style={{ transition: "color 0.3s ease" }}>
-              <FaShoppingCart /> 
-            </Nav.Link>
-          </LinkContainer>
+          <Nav className="d-flex align-items-center">
+            <LinkContainer to="/wishlist">
+              <Nav.Link className="text-white mx-3" style={{ transition: "color 0.3s ease" }}>
+                <FaHeart />
+              </Nav.Link>
+            </LinkContainer>
 
-          <LinkContainer to="/login">
-            <Nav.Link className="text-white mx-4" style={{ transition: "color 0.3s ease" }}>
-              <FaUser /> 
-            </Nav.Link>
-          </LinkContainer>
+            <LinkContainer to="/notifications">
+              <Nav.Link className="text-white mx-3" style={{ transition: "color 0.3s ease" }}>
+                <FaBell />
+              </Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/cart">
+              <Nav.Link className="text-white mx-3" style={{ transition: "color 0.3s ease" }}>
+                <FaShoppingCart />
+              </Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/login">
+              <Nav.Link className="text-white mx-3" style={{ transition: "color 0.3s ease" }}>
+                <FaUser />
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
