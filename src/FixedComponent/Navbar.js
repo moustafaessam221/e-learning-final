aed117b99e73338d23ab5d7dcfe9c62e812c903d
Sherif Othman <sherif.othman@sci.alexu.edu.eg>
2024-09-25 13:35:35 +0100

    Update Navbar component with new changes

--- src/FixedComponent/Navbar.js
+++ src/FixedComponent/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaUser, FaShoppingCart } from "react-icons/fa"; 
 import SearchBar from "./Search";
 
 const Navigationbar = () => {
@@ -18,42 +19,27 @@ const Navigationbar = () => {
         <Navbar.Collapse id="navbarScroll">
           <Nav className="ms-auto my-2 my-lg-0" navbarScroll>
             <LinkContainer to="/">
-              <Nav.Link
-                className="text-white"
-                style={{ transition: "color 0.3s ease" }}
-              >
+              <Nav.Link className="text-white" style={{ transition: "color 0.3s ease" }}>
                 Home
               </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/about">
-              <Nav.Link
-                className="text-white"
-                style={{ transition: "color 0.3s ease" }}
-              >
+              <Nav.Link className="text-white" style={{ transition: "color 0.3s ease" }}>
                 About
               </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/courses">
-              <Nav.Link
-                className="text-white"
-                style={{ transition: "color 0.3s ease" }}
-              >
+              <Nav.Link className="text-white" style={{ transition: "color 0.3s ease" }}>
                 Courses
               </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/profile">
-              <Nav.Link
-                className="text-white"
-                style={{ transition: "color 0.3s ease" }}
-              >
+              <Nav.Link className="text-white" style={{ transition: "color 0.3s ease" }}>
                 Profile
               </Nav.Link>
             </LinkContainer>
             <LinkContainer to="/contact">
-              <Nav.Link
-                className="text-white"
-                style={{ transition: "color 0.3s ease" }}
-              >
+              <Nav.Link className="text-white" style={{ transition: "color 0.3s ease" }}>
                 Contact
               </Nav.Link>
             </LinkContainer>
@@ -63,12 +49,15 @@ const Navigationbar = () => {
             <SearchBar />
           </div>
 
+          <LinkContainer to="/cart">
+            <Nav.Link className="text-white mx-2" style={{ transition: "color 0.3s ease" }}>
+              <FaShoppingCart /> 
+            </Nav.Link>
+          </LinkContainer>
+
           <LinkContainer to="/login">
-            <Nav.Link
-              className="text-white mx-4"
-              style={{ transition: "color 0.3s ease" }}
-            >
-              Login
+            <Nav.Link className="text-white mx-4" style={{ transition: "color 0.3s ease" }}>
+              <FaUser /> 
             </Nav.Link>
           </LinkContainer>
         </Navbar.Collapse>
